c94b996967c03f4e13e9d42d5ccc6411fb74c393
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-24 01:16:15 +0200

    Add task 01(SimpleLightbox)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//  console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const galleryList = document.querySelector('.gallery');
+const galleryListItems = createGaleryItem(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryListItems);
+
+function createGaleryItem(galleryItems) {
+  const galleryItem = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li> `;
+    })
+    .join('');
+
+  return galleryItem;
+}
+
+var lightbox = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
