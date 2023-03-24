429b60ad3e7e94cc57c32486e62bf2f0edee98c6
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-24 15:07:33 +0200

    Add task 03(feedback)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+let feedbackForm = {};
+
+form.addEventListener('input', throttle(saveMessage, 1000));
+form.addEventListener('submit', onFormSubmit);
+
+populateFeedbackForm();
+
+function saveMessage(evt) {
+  evt.preventDefault();
+  feedbackForm[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  //   console.log('go!');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateFeedbackForm() {
+  const savedFeedbackForm = localStorage.getItem(STORAGE_KEY);
+  if (savedFeedbackForm) {
+    feedbackForm = JSON.parse(savedFeedbackForm);
+    let { email, message } = form.elements;
+    if (feedbackForm.email) {
+      email.value = feedbackForm.email;
+    }
+    if (feedbackForm.message) {
+      message.value = feedbackForm.message;
+    }
+    // console.log(feedbackForm);
+  }
+}
